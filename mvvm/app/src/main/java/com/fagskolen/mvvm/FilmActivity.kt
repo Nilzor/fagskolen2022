70bbd0d7897e7daf75fb2bd9c0e33e22be2ec8b6
Frode Nilsen <nilzor@gmail.com>
2022-03-01 12:55:51 +0100

    Simulate slow data loading - with progress bar

--- mvvm/app/src/main/java/com/fagskolen/mvvm/FilmActivity.kt
+++ mvvm/app/src/main/java/com/fagskolen/mvvm/FilmActivity.kt
@@ -1,9 +1,13 @@
 package com.fagskolen.mvvm
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -11,18 +15,24 @@ import kotlinx.coroutines.launch
 class FilmActivity : AppCompatActivity() {
     lateinit var filmViewModel: FilmViewModel
 
+    private lateinit var progressBar: ProgressBar
+    private lateinit var filmContent: ConstraintLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_film)
         supportActionBar?.hide()
 
+        progressBar = findViewById(R.id.progressBar)
+        filmContent = findViewById(R.id.filmContent)
+
         filmViewModel = FilmViewModel()
         loadDataIfNeeded()
     }
 
-
     private fun loadDataIfNeeded() {
         if (!filmViewModel.isLoaded()) {
+            Log.d("Film1", "Preparing to load data")
             loadDataInBackground()
         } else {
             show(filmViewModel)
@@ -31,10 +41,18 @@ class FilmActivity : AppCompatActivity() {
 
     private fun loadDataInBackground() {
         lifecycleScope.launch {
+            Log.d("Film1", "Loading data on background thread")
+
+            progressBar.visibility = View.VISIBLE
+            filmContent.visibility = View.GONE
+
             delay(2000) // Vi later som om datakilden er treg
 
             val filmModel = DataStorage.loadFilm()
             Converters.filmModelToViewModel(filmModel, filmViewModel)
+
+            progressBar.visibility = View.GONE
+            filmContent.visibility = View.VISIBLE
 
             show(filmViewModel)
         }
