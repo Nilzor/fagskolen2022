4ce6da0b4768b42d263eabd3e11d044365690492
Frode Nilsen <nilzor@gmail.com>
2022-05-09 07:45:04 +0200

    Unit testing

---
+++ unittesting/app/src/main/java/com/fagskolen/unittesting/StringUtils.kt
@@ -0,0 +1,50 @@
+package com.fagskolen.unittesting
+
+class StringUtils {
+    val vowels = "aeiouyæøå"
+
+    fun toAlternatingCase(input: String): String {
+        var result = ""
+        for (i in input.indices) {
+            val c: Char = input[i]
+            if (i % 2 == 0) result += c.uppercase()
+            else result += c.lowercase()
+        }
+        return result
+    }
+
+    fun isVowel(input: Char): Boolean {
+        return vowels.contains(input)
+    }
+
+    /**
+     * The numbers must be separated by + sign and contain no other characters
+     * Letters or other symbols will throw exception
+     *
+     * Example: addIntegerNumbers("1+5+14") // will return 20
+     */
+    fun addIntegerNumbers(input: String): Int {
+        if (input.isEmpty()) return 0
+        val elements = input.split("+")
+        var result = 0
+        for (numberAsString in elements) {
+            val numberAsInt = numberAsString.toInt()
+            result += numberAsInt
+        }
+        return result
+    }
+
+    fun rot13(input: String): String {
+        var result = "" // 65 - 90
+        for (i in input.indices) {
+            val c = input[i]
+            if (c.code < 65 || c.code > 90) {
+                throw IllegalArgumentException("Character '$c' is not accepted input")
+            }
+            var resCode = c.code + 13
+            if (resCode > 90) resCode -= 26
+            result += Char(resCode)
+        }
+        return result
+    }
+}
---
+++ unittesting/app/src/test/java/com/fagskolen/unittesting/StringUtilsTest.kt
@@ -0,0 +1,101 @@
+package com.fagskolen.unittesting
+
+
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.rules.ExpectedException
+import java.lang.Exception
+import java.lang.IndexOutOfBoundsException
+import java.lang.NumberFormatException
+
+internal class StringUtilsTest {
+
+    @Test
+    fun `uppercase when already upper case`() {
+        val input = "HEI DU"
+        val result = input.uppercase()
+        assertEquals("HEI DU", result)
+    }
+
+    @Test
+    fun `uppercase when lower case`() {
+        val input = "hei du"
+        val result = input.uppercase()
+        assertEquals("HEI DU", result)
+
+    }
+
+    @Test
+    fun `uppercase when mixed case`() {
+        val input = "Hei Du"
+        val result = input.uppercase()
+        assertEquals("HEI DU", result)
+    }
+
+    @Test
+    fun `alternatingcase - long text`() {
+        val input = "god dag på deg"
+        val result = StringUtils().toAlternatingCase(input)
+        assertEquals("GoD DaG På dEg", result)
+    }
+
+    @Test
+    fun `alternatingcase - empty string`() {
+        val input = ""
+        val result = StringUtils().toAlternatingCase(input)
+        assertEquals("", result)
+    }
+
+    @Test
+    fun `isVowel - a`() {
+        val result = StringUtils().isVowel('a')
+        assertTrue(result)
+    }
+
+    @Test
+    fun `isVowel - b`() {
+        val result = StringUtils().isVowel('b')
+        assertFalse(result)
+    }
+
+    @Test
+    fun `addIntegerNumbers - one number`() {
+        val res = StringUtils().addIntegerNumbers("4")
+        assertEquals(4, res)
+    }
+
+    @Test
+    fun `addIntegerNumbers - two numbers`() {
+        val res = StringUtils().addIntegerNumbers("4+1")
+        assertEquals(5, res)
+    }
+
+    @Test
+    fun `addIntegerNumbers - three numbers`() {
+        val res = StringUtils().addIntegerNumbers("11+5+17")
+        assertEquals(33, res)
+    }
+
+    @Test(expected = Exception::class)
+    fun `addIntegerNumbers - one number and some text`() {
+        StringUtils().addIntegerNumbers("11+kake")
+    }
+
+    @Test(expected = Exception::class)
+    fun `addIntegerNumbers - two number separated by minus`() {
+        StringUtils().addIntegerNumbers("11-4")
+    }
+
+    @Test
+    fun `addIntegerNumbers - empty string`() {
+        val res = StringUtils().addIntegerNumbers("")
+        assertEquals(0, res)
+    }
+
+    @Test
+    fun `rot13 - KAKE`() {
+        val input = listOf(1.0, 2.0)
+        val result = input.average()
+        assertEquals(1.5, result, 0.1)
+    }
+}
