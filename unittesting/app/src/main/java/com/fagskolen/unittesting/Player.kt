e3fda3691969f6f8c830eb715b5769149b435f12
Frode Nilsen <nilzor@gmail.com>
2022-05-09 15:46:10 +0200

    TDD when done

--- unittesting/app/src/main/java/com/fagskolen/unittesting/Player.kt
+++ unittesting/app/src/main/java/com/fagskolen/unittesting/Player.kt
@@ -5,6 +5,22 @@ data class Player(
     val scores: List<Score>?
 ) {
     fun get18Scores(): List<Score> {
-        return scores!!
+        val outList = mutableListOf<Score>()
+        if (scores == null) {
+            for (i in 1..18) {
+                outList.add(Score(i, 0))
+            }
+        } else {
+            for (i in 1..18) {
+                val foundScore = scores.find { it.hole == i }
+                if (foundScore != null) {
+                    outList.add(foundScore)
+                } else {
+                    outList.add(Score(i, 0))
+                }
+            }
+        }
+
+        return outList
     }
 }
--- unittesting/app/src/test/java/com/fagskolen/unittesting/PlayerTest.kt
+++ unittesting/app/src/test/java/com/fagskolen/unittesting/PlayerTest.kt
@@ -15,4 +15,64 @@ internal class PlayerTest {
         val result = player.get18Scores()
         assertEquals(18, result.size)
     }
+
+    @Test
+    fun get18Scores_two_and_five_whole_two_correct() {
+        val player = Player(
+            "Frode",
+            scores = listOf(
+                Score(2, 3),
+                Score(5, 7)
+            )
+        )
+
+        val result = player.get18Scores()
+
+        assertEquals(3, result[1].score)
+    }
+
+    @Test
+    fun get18Scores_two_and_five_hole_five_correct() {
+        val player = Player(
+            "Frode",
+            scores = listOf(
+                Score(2, 3),
+                Score(5, 7)
+            )
+        )
+
+        val result = player.get18Scores()
+
+        assertEquals(7, result[4].score)
+    }
+
+    @Test
+    fun get18Scores_two_and_five_hole_size_correct() {
+        val player = Player(
+            "Frode",
+            scores = listOf(
+                Score(2, 3),
+                Score(5, 7)
+            )
+        )
+
+        val result = player.get18Scores()
+
+        assertEquals(18, result.size)
+    }
+
+    @Test
+    fun get18Scores_hole_seven_is_zero() {
+        val player = Player(
+            "Frode",
+            scores = listOf(
+                Score(2, 3),
+                Score(5, 7)
+            )
+        )
+
+        val result = player.get18Scores()
+
+        assertEquals(0, result[6].score)
+    }
 }
